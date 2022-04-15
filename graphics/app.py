e44f2f00a7d6bd58974cac3c08022a3569b5866e
PrzestrzelMarek <PrzestrzelMarek@github.com>
2022-04-15 17:07:50 +0200

    Refactor for future socket connection

--- graphics/app.py
+++ graphics/app.py
@@ -4,9 +4,15 @@ from graphics.chat_page import ChatPage
 
 
 class ChatApp(App):
-    def build(self):
+
+    def __init__(self, **kwargs):
+        super(ChatApp, self).__init__(**kwargs)
         Window.clearcolor = (0.1, 0.1, 0.1, 0.9)
         Window.size = (1024, 768)
         self.title = 'SafeChatApp'
-        return ChatPage()
+        self.chat_page = ChatPage()
+        self.history = self.chat_page.history
+
+    def build(self):
+        return self.chat_page
 
