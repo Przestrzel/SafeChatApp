bcb068459e462d05ead4552710f5cbe1c7f1f9b7
Przestrzel <marek.przestrzelski00@gmail.com>
2022-06-04 19:27:10 +0200

    Change keys

--- encryption/RSA_key_generator.py
+++ encryption/RSA_key_generator.py
@@ -6,18 +6,18 @@ class RSAKeygen:
     @staticmethod
     def generate_keys(client_name):
         (pub_key, priv_key) = rsa.newkeys(1024)
-        with open(f'pubkey_{client_name}.pem', 'wb') as f:
+        with open(f'keys/{client_name}/pubkey.pem', 'wb') as f:
             f.write(pub_key.save_pkcs1('PEM'))
 
-        with open(f'privkey_{client_name}.pem', 'wb') as f:
+        with open(f'keys/{client_name}/privkey.pem', 'wb') as f:
             f.write(priv_key.save_pkcs1('PEM'))
 
     @staticmethod
     def load_keys(client_name):
-        with open(f'pubkey_{client_name}.pem', 'rb') as f:
+        with open(f'keys/{client_name}/pubkey.pem', 'rb') as f:
             pubKey = rsa.PublicKey.load_pkcs1(f.read())
 
-        with open(f'privkey_{client_name}.pem', 'rb') as f:
+        with open(f'keys/{client_name}/privkey.pem', 'rb') as f:
             privKey = rsa   .PrivateKey.load_pkcs1(f.read())
 
         return pubKey, privKey
